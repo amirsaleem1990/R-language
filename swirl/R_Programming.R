a64dd8b1bd401b824caf71ac5c35f33eda378abd
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-20 00:36:27 +0500

    updated

--- swirl/R_Programming.R
+++ swirl/R_Programming.R
@@ -88,3 +88,42 @@ mad_libs <- function(...){
 # [1] "News from AAAAAAAA today where BBBBBBB students took to the streets in protest of the new CCCCCCCCCC being installed on campus."
 
 
+
+
+# The syntax for creating new binary operators in R is unlike anything else in
+# R, but it allows you to define a new syntax for your function. I would only
+# recommend making your own binary operator if you plan on using it often!
+#
+# User-defined binary operators have the following syntax:
+#      %[whatever]% 
+# where [whatever] represents any valid variable name.
+# 
+# Let's say I wanted to define a binary operator that multiplied two numbers and
+# then added one to the product. An implementation of that operator is below:
+#
+# "%mult_add_one%" <- function(left, right){ # Notice the quotation marks!
+#   left * right + 1
+# }
+#
+# I could then use this binary operator like `4 %mult_add_one% 5` which would
+# evaluate to 21.
+#
+# Write your own binary operator below from absolute scratch! Your binary
+# operator must be called %p% so that the expression:
+#
+#       "Good" %p% "job!"
+#
+# will evaluate to: "Good job!"
+
+"%p%" <- function(l, r){ # Remember to add arguments!
+  paste(l, r)
+}
+
+"Good" %p% "job!"
+# [1] "Good job!"
+
+> "I" %p% "love" %p% "R!"
+# [1] "I love R!"
+
+
+
