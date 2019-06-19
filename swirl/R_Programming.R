a6c10da6c476180eeb3d4a4c1b5e5542af57e817
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-19 23:28:53 +0500

    updated

--- swirl/R_Programming.R
+++ swirl/R_Programming.R
@@ -2,3 +2,12 @@ library(swirl)
 swirl()
 
 8: Logic
+> TRUE & c(TRUE, FALSE, FALSE)
+# [1]  TRUE FALSE FALSE
+
+
+# the left operand is only evaluated with the first member of the right operand (the vector). The rest of the elements in the vector aren't evaluated at all in this expression.
+> TRUE && c(TRUE, FALSE, FALSE)
+# [1] TRUE
+
+
