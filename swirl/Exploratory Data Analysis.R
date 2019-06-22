c7b2022a3674e239856f16dacec1311d1863d35f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-22 17:15:29 +0500

    updated

--- swirl/Exploratory Data Analysis.R
+++ swirl/Exploratory Data Analysis.R
@@ -6,3 +6,5 @@
 
 # When presented with a claim that something is good, you should always ask "Compared to What?" This is why in commercials you often hear the phrase "other leading brands". An implicit comparison, right?
 
+# the first principle was to show a comparison. The second principle is to show causality or a mechanism of how your theory of the data works. This explanation or systematic structure shows your causal framework for thinking about the question you're trying to answer.
+
