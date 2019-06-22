e8b15f5e235f047b70bb83740695a7bc53dd4c15
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-22 15:36:20 +0500

    updated

---
+++ swirl/Exploratory Data Analysis.R
@@ -0,0 +1,8 @@
+1: Principles of Analytic Graphs
+
+# Graphs give us a visual form of data, and the first principle of analytic graphs is to show some comparison. You'll hear more about this when you study statistical inference (another great course BTW), but evidence for a hypothesis is always relative to another competing or alternative hypothesis.
+
+
+
+# When presented with a claim that something is good, you should always ask "Compared to What?" This is why in commercials you often hear the phrase "other leading brands". An implicit comparison, right?
+
