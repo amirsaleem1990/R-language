ad0d909293eb506c142edee5769ab9746ec582b5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-07-26 15:24:22 +0500

    updated

---
+++ swirl/temp.R
@@ -0,0 +1,12 @@
+> shape_mat <- sapply(flag_shapes, range)
+> shape_mat
+# circles crosses saltires quarters sunstars
+# [1,]       0       0        0        0        0
+# [2,]       4       2        1        4       50
+
+
+# confirm that shape_mat is a matrix.
+> class(shape_mat)
+# [1] "matrix"
+
+
