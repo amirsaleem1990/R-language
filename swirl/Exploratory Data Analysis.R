727c8deac3c700be2ea482e06c18e50aaa75d2ad
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-05 12:37:57 +0500

    updated

--- swirl/Exploratory Data Analysis.R
+++ swirl/Exploratory Data Analysis.R
@@ -977,3 +977,4 @@ a1 <- (svd1$u[,1] * svd1$d[1]) %*% t(svd1$v[,1])
   plot( hclust, labels=FALSE, hang=hang, ... )
   text( x=x, y=y[hclust$order]-(max(hclust$height)*hang), labels=lab[hclust$order], col=lab.col[hclust$order], srt=90, adj=c(1,0.5), xpd=NA, ... )}
 
+> myplclust(hclustering, lab.col = unclass(sub1$activity))
