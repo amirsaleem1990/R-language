d07d0d3d20f5d81c0f27b803821bc80e20940e35
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-05 12:04:04 +0500

    updated

--- swirl/Exploratory Data Analysis.R
+++ swirl/Exploratory Data Analysis.R
@@ -902,3 +902,4 @@ a1 <- (svd1$u[,1] * svd1$d[1]) %*% t(svd1$v[,1])
 ##########################################################################
 # 14: Clustering Example                                                 #
 ##########################################################################
+# Slides for this and other Data Science courses may be found at github https://github.com/DataScienceSpecialization/courses/. If you care to use them, they must be downloaded as a zip file and viewed locally. This lesson corresponds to 04_ExploratoryAnalysis/clusteringExample
