69be0bee5edea7d49029d846af527dca2815f09e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-05 10:09:49 +0500

    updated

--- swirl/Exploratory Data Analysis.R
+++ swirl/Exploratory Data Analysis.R
@@ -678,4 +678,5 @@ g + geom_point(aes(color = drv)) + theme_bw(base_family =  "Times")
 ###########################################################################################################
 # 13: Dimension Reduction                                                                                 #
 ###########################################################################################################
+# Slides for this and other Data Science courses may be found at github https://github.com/DataScienceSpecialization/courses/. If you care to use them, they must be downloaded as a zip file and viewed locally. This lesson corresponds to 04_ExploratoryAnalysis/dimensionReduction
 
