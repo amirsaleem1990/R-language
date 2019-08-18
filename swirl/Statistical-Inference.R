75ace46d133d1691cf620f1f392f1e9fce1b907f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-18 19:08:19 +0500

    updated

--- swirl/Statistical-Inference.R
+++ swirl/Statistical-Inference.R
@@ -140,4 +140,8 @@
 
 
 
+###################################################################
+# 4: ConditionalProbability                                       #
+###################################################################
 
+# Slides for this and other Data Science courses may be found at github https://github.com/DataScienceSpecialization/courses/. If you care to use them, they must be downloaded as a zip file and viewed locally. This lesson corresponds to 06_Statistical_Inference/03_Conditional_Probability
