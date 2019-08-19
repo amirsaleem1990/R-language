9ef3adccbda7d0d78b61aff54c9f5397997c1ed0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-19 12:43:16 +0500

    updated

--- swirl/Statistical-Inference.R
+++ swirl/Statistical-Inference.R
@@ -224,3 +224,4 @@
 # 5: Expectations                                                                                    #
 ######################################################################################################
 
+# Slides for this and other Data Science courses may be found at github https://github.com/DataScienceSpecialization/courses/. If you care to use them, they must be downloaded as a zip file and viewed locally. This lesson corresponds to 06_Statistical_Inference/04_Expectations.
