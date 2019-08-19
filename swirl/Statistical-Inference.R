a77a6e42f927b18cd89a307fa25de02081522b15
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-19 14:17:55 +0500

    updated

--- swirl/Statistical-Inference.R
+++ swirl/Statistical-Inference.R
@@ -260,6 +260,10 @@
 
 # For the purposes of illustration, here's(image-4.png) another figure using a PDF from our previous probability lesson. It shows the triangular PDF f(t) on the left and the parabolic t*f(t) on the right. The area under the parabola between 0 and 2 represents the expected value of the random variable with this PDF.
 # To find the expected value of this random variable you need to integrate the function t*f(t). Here f(t)=t/2, the diagonal line. (You might recall this from the last probability lesson.) The function you're integrating over is therefore t^2/2. We've defined a function myfunc for you representing this. You can use the R function 'integrate' with parameters myfunc, 0 (the lower bound), and 2 (the upper bound) to find the expected value. Do this now.
+> myfunc
+# function(x){x^2/2}
+
 > integrate(myfunc, lower = 0, upper = 2)
 # 1.333333 with absolute error < 1.5e-14
 
+# As all the examples have shown, expected values of distributions are useful in characterizing them. The mean characterizes the central tendency of the distribution. However, often populations are too big to measure, so we have to sample them and then we have to use sample means. That's okay because sample expected values estimate the population versions. We'll show this first with a very simple toy and then with some simple equations.
