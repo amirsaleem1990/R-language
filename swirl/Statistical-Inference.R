3bb2fe0b86339e1210759fb41eebbe5589851f37
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-20 11:19:21 +0500

    updated

--- swirl/Statistical-Inference.R
+++ swirl/Statistical-Inference.R
@@ -771,4 +771,4 @@ pbinom(2, size = 5, prob = 0.8, lower.tail = FALSE)
 #############################################################################
 # 9: T Confidence Intervals                                                 #
 #############################################################################
-
+# Slides for this and other Data Science courses may be found at github https://github.com/DataScienceSpecialization/courses/. If you care to use them, they must be downloaded as a zip file and viewed locally. This lesson corresponds to 06_Statistical_Inference/08_tCIs
